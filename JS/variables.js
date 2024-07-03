ac305c3b4cd9d57e757f9b2b18e0466bd2f50663
TobiasLopezD <entreprisetobys@outlook.com>
2024-07-02 20:01:31 -0600

    Feat: Variables JS

---
+++ JS/variables.js
@@ -0,0 +1,36 @@
+
+// .btn {
+//      color:white ;  
+
+// }
+
+// function btn () {
+
+
+// }
+
+// Lenguaje de programacion
+
+// Javascript NO ES LO MISMO QUE JAVA
+
+
+// Mostrar un mensaje en consola del navegador
+// console.log ("Hola mundo");
+
+
+//1. Variables  Almacenan datos var = let
+// const PI = 3,14 example
+
+// let nombre = "Hola Mundo"
+
+// console.log (nombre)
+
+let Nombre = "Tobias"
+let Apellido = "Lopez Douglas"
+let Edad = 29
+let Direccion = " Urbanizacion vista alegre, Escazu"
+
+console.log(Nombre);
+console.log(Apellido);
+console.log(Edad);
+console.log(Direccion);
