0aeedc6cbeab12c5585966dd2bc9c0351a6f4fd8
TobiasLopezD <entreprisetobys@outlook.com>
2024-07-02 21:25:12 -0600

    Feat: Variables JS

--- JS/variables.js
+++ JS/variables.js
@@ -9,25 +9,33 @@
 
 // }
 
-// Lenguaje de programacion
+// Lenguaje de proogramacion
 
 // Javascript NO ES LO MISMO QUE JAVA
 
 
-// Mostrar un mensaje en consola del navegador
-// console.log ("Hola mundo");
 
-
-//1. Variables  Almacenan datos var = let
+//1. Variables  Almacenan datos var = let, const
 // const PI = 3,14 example
 
-// let nombre = "Hola Mundo"
-
-// console.log (nombre)
-
+// Zona Para declarar variables
 let Nombre = "Tobias"
 let Apellido = "Lopez Douglas"
 let Edad = 29
 let Direccion = " Urbanizacion vista alegre, Escazu"
+let Mensaje = "Hola mi nombre es: "+Nombre+" Y mis apellidos son "+Apellido+" mi edad es "+Edad+" vivo en "+Direccion+""
+let Mensaje2 = "Es usted mayor de edad ?"
+let ventana
 
-console.log("Hola mi nombre es: "+Nombre+" Y mis apellidos son "+Apellido+" mi edad es "+Edad+" vivo en "+Direccion+"");
+
+
+
+// Fin de zona para declarar variables
+
+
+// Mostar un mensaje en consola del navegador
+console.log(Mensaje);
+// Capturar lo que digita el usuario
+Ventana = prompt (Mensaje2)
+// Mostrar un mensaje en una ventana emergente
+alert (Ventana)
