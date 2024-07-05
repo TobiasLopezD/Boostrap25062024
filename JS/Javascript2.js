05a6ebf1191d23ff7e57c84af7e5c2183ce9a04b
TobiasLopezD <entreprisetobys@outlook.com>
2024-07-04 20:03:45 -0600

    Feat: Javascript 2

--- JS/Javascript2.js
+++ JS/Javascript2.js
@@ -30,4 +30,69 @@ let Lambo = {
     Especificaciones: [ "Negro", "Dorado", "Nitro", "Turbo"]
 }
 
-console.log(Lambo.HP)
+console.log(Lambo.Rapido)
+
+// Profe resolucion
+
+const  Auto = {
+    Marca: "Toyota",
+    Puertas: 4,
+    Extras: true,
+    Capacidad: [1,2,4]
+}
+
+console.table (Auto);
+console.error (Auto)
+console.warn(Auto)
+
+let Mensaje = "La Pais del auto es "+Lambo.Pais+" tiene "+Lambo.HP+" Caballos de fuerza"
+
+//Patron literal
+let Mensaje2 = `La marca del auto es ${Auto.Marca} posee la cantidad de puertas, ${Auto.Puertas}`
+
+console.table(Mensaje2)
+
+// 3 Operadores 
+//Aritmeticos
+
+let suma = 5 + 3
+let resta = 3 - 5
+let multi = 5 * 2
+let div = 8 / 2
+let mod = 10%3 
+
+//Comparacion
+console.log(5 == 5)
+console.log(5 == "5") // == igualdad debil
+console.log(5 === "5") // === igualdad fuerte
+console.log(5 != "5") // false
+console.log(5 !== "5") // True
+console.log(5 > 3) // True
+console.log(5 < 1) // false
+
+//Logicos
+//&& = AND
+// || = OR
+
+console.log (true && false)
+console.log (true || false)
+
+// 4-  Estructuras de control
+
+let pato = true
+if (pato = true) { //SI
+    alert ("Pato" +pato)
+
+} else { // Si no
+    alert ("no cumple")
+
+}
+
+
+let edad = 18
+
+if ( edad >= 18) {
+    alert("es mayor de edad")
+} else {
+    alert( "No es mayor de edad")
+}
