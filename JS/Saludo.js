ef4a0ef39d60efc60aee7ffaa29530e63557ba48
TobiasLopezD <entreprisetobys@outlook.com>
2024-07-09 19:49:44 -0600

    Feat: Clase 07/09/2024

---
+++ JS/Saludo.js
@@ -0,0 +1,33 @@
+console.log ("Funcionando")
+
+let Mensaje = "Hola Mundo"
+
+// Console.log - Mostrar
+//Alert - Mostrar
+//Prompt  - Ingresar
+
+console.log (Mensaje)
+
+//resultado
+
+//DOM
+// Document Object Model
+
+// Seleccionar el elemento del DOM por el ID y cambiar el contenido
+
+//document obtenerporid (Aqui ID).Propiedad = Valor
+// document.getElementById ("resultado").textContent = "Hola Mundo "
+
+// document.getElementById ("resultado").style.backgroundColor = "blue"
+
+// let DivResultado = document.getElementById ("resultado")
+
+// console.log (DivResultado)
+
+// DivResultado.textContent = "Hola Mundo"
+// DivResultado.style.backgroundColor = "blue"
+
+let TextResultado = document.getElementById ("text")
+TextResultado.textContent = "Contenido de texto"
+TextResultado.style.backgroundColor = "Green"
+TextResultado.style.color = "Yellow"
