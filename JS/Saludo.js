cae3f47461785caa8cb7ddf8dbae478eea935a7e
TobiasLopezD <entreprisetobys@outlook.com>
2024-07-09 20:18:35 -0600

    Feat: Clase 07/09/2024

--- JS/Saludo.js
+++ JS/Saludo.js
@@ -20,15 +20,29 @@ console.log (Mensaje)
 
 // document.getElementById ("resultado").style.backgroundColor = "blue"
 
-// let DivResultado = document.getElementById ("resultado")
+let DivResultado = document.getElementById ("resultado")
+console.log (DivResultado)
 
-// console.log (DivResultado)
-
-// DivResultado.textContent = "Hola Mundo"
-// DivResultado.style.backgroundColor = "blue"
+DivResultado.textContent = "Hola Mundo"
+DivResultado.style.backgroundColor = "blue"
+DivResultado.style.color = "White"
 
 let TextResultado = document.getElementById ("text")
 console.log (TextResultado)
 TextResultado.textContent = "Contenido de texto"
 TextResultado.style.backgroundColor = "Green"
 TextResultado.style.color = "Yellow"
+
+//debugger
+//Obtener el valor actual del DOM
+let Titulo = document.getElementById ("Titulo").value;
+// Titulo.textContent = "Gato"
+// Titulo.innerHTML = "Perro"
+
+console.log (Titulo)
+
+let nuevoparrafo = document.createElement ("p")
+nuevoparrafo.textContent = "Contenido de la etiqueta"
+// Agregar Etiquetas CreateElement al HTML
+DivResultado.appendChild(nuevoparrafo)
+console.log (nuevoparrafo)
