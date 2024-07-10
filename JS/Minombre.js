2fe58882daadaf83335e818d4d868d40cd9ef87d
TobiasLopezD <entreprisetobys@outlook.com>
2024-07-09 21:20:52 -0600

    Feat: programacion formulario

--- JS/Minombre.js
+++ JS/Minombre.js
@@ -1,1 +1,19 @@
+//Formulario
+//Nombre y apellido
+//Y al darle click, Mostrar en un DIV
+
+
+
 console.log ("Funcionando")
+
+function Guardar() {
+    const formulario = document.getElementById ("formulario");
+    const valor1 = document.getElementById ("valor1").value;
+    const valor2 = document.getElementById ("valor2").value;
+    const valorrespuesta = document.getElementById ("valorrespuesta");
+
+    alert (`EL valor1 es ${valor1} el valor2 es ${valor2}`)
+    valorrespuesta.textContent = `EL valor1 es ${valor1} el valor2 es ${valor2}`
+
+    debugger
+}
