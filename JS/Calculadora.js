b92b5d1a4fc4ca35e09804ea0631b5f4cbe1e2f2
TobiasLopezD <entreprisetobys@outlook.com>
2024-07-11 21:36:00 -0600

    Feat: calculadora

--- JS/Calculadora.js
+++ JS/Calculadora.js
@@ -1,27 +1,58 @@
 console.log ("Funcionanado Calculadora")
 
-function Sumar(event) {
+//Global
+const Num1 = document.getElementById ("Num1")
+const Num2 = document.getElementById ("Num2")
+const Resultado = document.getElementById ("Resultado");
+
+function Calculadora2(event) {
     event.preventDefault();
-    const Num1 = document.getElementById ("Num1")
-    const Num2 = document.getElementById ("Num2")
-    const Resultado = document.getElementById ("Resultado");
+    const Resultado = document.getElementById ("Resultado");  
+    const Num1 = parseFloat(document.getElementById ("Num1").value);
+    const Num2 = parseFloat(document.getElementById ("Num2").value);
+    const operaciones =  document.getElementById ("operaciones").value;
+    let calculo
+    switch (operaciones ) {
+        case "+":
+                calculo = Num1 + Num2
+            break;
+        case "-":
+                calculo = Num1 - Num2
+            break;
+        case "*":
+                calculo = Num1 * Num2
+            break;
+        case "/":
+                calculo = Num1 / Num2
+            break;    
+        default:
+            calculo = "Operacion no valida"
+            break;
+    }
 
-    //parseflaot convierte string a decimal
-    //parseint convierte string a int
-    let calculo = parseFloat(Num1.value) + parseFloat(Num2.value);
-    Resultado.textContent = `El resultado es ${calculo}`
+    Resultado.textContent = `El resultado es:  ${calculo}`
 
+    console.log (operaciones)
 }
 
-function Restar(event) {
-    event.preventDefault();
-    const Num1 = document.getElementById ("Num1")
-    const Num2 = document.getElementById ("Num2")
-    const Resultado = document.getElementById ("Resultado");
+// function Sumar(event) {
+//     event.preventDefault();
 
-    //parseflaot convierte string a decimal
-    //parseint convierte string a int
-    let calculo = parseFloat(Num1.value) - parseFloat(Num2.value);
-    Resultado.textContent = `El resultado es ${calculo}`
 
-}
+//     //parseflaot convierte string a decimal
+//     //parseint convierte string a int
+//     let calculo = parseFloat(Num1.value) + parseFloat(Num2.value);
+//     Resultado.textContent = `El resultado es ${calculo}`
+
+// }
+
+// function Restar(event) {
+//     event.preventDefault();
+    
+
+//     //parseflaot convierte string a decimal
+//     //parseint convierte string a int
+//     let calculo = parseFloat(Num1.value) - parseFloat(Num2.value);
+//     Resultado.textContent = `El resultado es ${calculo}`
+
+// }
