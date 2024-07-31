20a89db1770751414bc0b511279e7eb90a320649
TobiasLopezD <entreprisetobys@outlook.com>
2024-07-30 20:14:39 -0600

    Feat: Arreglo de objetos Heroes alert

---
+++ Js/LigaDelaJusticia.js
@@ -0,0 +1,77 @@
+console.log("Funcionando");
+// 1-- Evento que carga la funcion cuando el DOM este en la pagina
+//Control + Space
+document.addEventListener('DOMContentLoaded', () => {
+    Mostrar_LigaDeLaJusticia (Heroes); // Cargue la pagina
+})
+// 2- Arreglo de objetos
+const Heroes = [ 
+    {
+      nombre: "Aquaman",
+      bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
+      img: "img/aquaman.png",
+      casa: "DC"
+    },
+    {
+      nombre: "Batman",
+      bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
+      img: "img/batman.png",
+      casa: "DC"
+    },
+    {
+      nombre: "Superman",
+      bio: "Superman es un superhéroe ficticio que aparece en los cómics publicados por DC Comics. Es uno de los personajes de ficción más populares del siglo XX.",
+      img: "img/superman.png",
+      casa: "DC"
+    },
+    {
+      nombre: "Wonder Woman",
+      bio: "Es una superheroína ficticia creada por el psicólogo y escritor William Moulton Marston para la editorial DC Comics. Sus historias se centran en los ideales del amor, paz y sexualidad.",
+      img: "img/wonder_woman.png",
+      casa: "DC"
+    }
+  ];
+
+
+// 3- Funcion quue muestra los Heroes
+//A  -ID del contenedor
+//B  -Plantilla
+//C  -Ciclo de la parte 2 (2- Arreglo de objetos)
+//D  -Meter la PLantilla en el contenedor
+function Mostrar_LigaDeLaJusticia(Heroes) { // Firma
+//     let contenedor //A
+   let Plantilla = "" ; //B
+//     //C
+Heroes.forEach(element => {
+    Plantilla = `
+        {
+      nombre: "Aquaman",
+      bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
+      img: "img/aquaman.png",
+      casa: "DC"
+    },
+    {
+      nombre: "Batman",
+      bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
+      img: "img/batman.png",
+      casa: "DC"
+    },
+    {
+      nombre: "Superman",
+      bio: "Superman es un superhéroe ficticio que aparece en los cómics publicados por DC Comics. Es uno de los personajes de ficción más populares del siglo XX.",
+      img: "img/superman.png",
+      casa: "DC"
+    },
+    {
+      nombre: "Wonder Woman",
+      bio: "Es una superheroína ficticia creada por el psicólogo y escritor William Moulton Marston para la editorial DC Comics. Sus historias se centran en los ideales del amor, paz y sexualidad.",
+      img: "img/wonder_woman.png",
+      casa: "DC"
+    }
+    `
+ });
+
+//     contenedor.innerHtm = Plantilla // D
+
+    alert (Plantilla)
+}
---
+++ Js/Plantilla.js
@@ -0,0 +1,22 @@
+// console.log("Funcionando");
+// // 1-- Evento que carga la funcion cuando el DOM este en la pagina
+// //Control + Space
+// document.addEventListener('DOMContentLoaded', () => {
+//     Mostrar_LigaDeLaJusticia ();
+// })
+// // 2- Arreglo de objetos
+// const Heroes
+
+
+// // 3- Funcion quue muestra los Heroes
+// //A  -ID del contenedor
+// //B  -Plantilla
+// //C  -Ciclo de la parte 2 (2- Arreglo de objetos)
+// //D  -Meter la PLantilla en el contenedor
+// function Mostrar_LigaDeLaJusticia(Heroes) {
+//     let contenedor //A
+//     let Plantilla //B
+//     //C
+
+//     contenedor.innerHtm = Plantilla // D
+// }
