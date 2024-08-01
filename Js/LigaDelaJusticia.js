eae1324f73106f22f224a1ce85b91609ab6bb12f
TobiasLopezD <entreprisetobys@outlook.com>
2024-08-01 14:49:29 -0600

    Clase laboratorio 07302024

--- Js/LigaDelaJusticia.js
+++ Js/LigaDelaJusticia.js
@@ -1,11 +1,15 @@
-console.log("Funcionando");
-// 1-- Evento que carga la funcion cuando el DOM este en la pagina
+console.log("funcionando")
+// 1--Evento que carga la funcion cuando el DOM este en la pagina 
 //Control + Space
 document.addEventListener('DOMContentLoaded', () => {
-    Mostrar_LigaDeLaJusticia (Heroes); // Cargue la pagina
+    
+    Mostrar_LigaDeLaJusticia(Heroes); //Carge la pagina  
+
 })
+
+
 // 2- Arreglo de objetos
-const Heroes = [ 
+const Heroes = [
     {
       nombre: "Aquaman",
       bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
@@ -32,46 +36,36 @@ const Heroes = [
     }
   ];
 
-
-// 3- Funcion quue muestra los Heroes
-//A  -ID del contenedor
+// 3-Funcion que muestra los heroes 
+//A  -ID del contendor
 //B  -Plantilla
-//C  -Ciclo de la parte 2 (2- Arreglo de objetos)
-//D  -Meter la PLantilla en el contenedor
-function Mostrar_LigaDeLaJusticia(Heroes) { // Firma
-//     let contenedor //A
-   let Plantilla = "" ; //B
-//     //C
-Heroes.forEach(element => {
-    Plantilla = `
-        {
-      nombre: "Aquaman",
-      bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
-      img: "img/aquaman.png",
-      casa: "DC"
-    },
-    {
-      nombre: "Batman",
-      bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
-      img: "img/batman.png",
-      casa: "DC"
-    },
-    {
-      nombre: "Superman",
-      bio: "Superman es un superhéroe ficticio que aparece en los cómics publicados por DC Comics. Es uno de los personajes de ficción más populares del siglo XX.",
-      img: "img/superman.png",
-      casa: "DC"
-    },
-    {
-      nombre: "Wonder Woman",
-      bio: "Es una superheroína ficticia creada por el psicólogo y escritor William Moulton Marston para la editorial DC Comics. Sus historias se centran en los ideales del amor, paz y sexualidad.",
-      img: "img/wonder_woman.png",
-      casa: "DC"
-    }
-    `
- });
+//C  -Ciclo de la parte 2 (2- Arreglo de objetos) 
+//D Meter la Plantilla en el contenedor 
 
-//     contenedor.innerHtm = Plantilla // D
+function Mostrar_LigaDeLaJusticia(Heroes) { //Firma
+    // let Contenedor //A
+    let Contenedor = document.getElementById ("contenedorHeroes")
+    let Plantilla = " "//B
+    // //C 
+    Heroes.forEach(element => {
+        Plantilla += `
+        <div class = "col-lg-4 mb-4"">
+            <div class="card h-100" >
+                <img class="card-img-top" src="${element.img}" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">${element.nombre}</h4>
+                    <p class="card-text">${element.bio}</p>
+                    <button type="button" name="" id="" class="btn btn-primary" btn-lg btn-block">Ver mas </button>
+                </div>
+            </div>
+        </div>
+        `
+    });
 
-    alert (Plantilla)
+    // Contenedor.innerHtml = Plantilla //D
+    Contenedor.innerHTML = Plantilla
+    console.log(Heroes)
 }
+
+//JSON 
+//String 
--- Js/NavBar.js
+++ Js/NavBar.js
@@ -1,98 +1,73 @@
-console.log("funcionando")
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    Mostrar_NavBar(); 
+ 
+ 
+ })
+ 
+ 
+ const ListadoMenu  = [
+     {
+         Nombre: "Inicio",
+         Ruta:"Index.html"       
+     },
+     {
+         Nombre: "Listado de Productos",
+         Ruta:"List-Products.html"       
+     },
+     {
+         Nombre: "Crear Producto",
+         Ruta:"New-Product.html"       
+     },
+     {
+         Nombre: "Pagina de Mercadeo",
+         Ruta:""       
+     }
+ ]
+ 
+ 
+ function Mostrar_NavBar() {
+  
+ console.log(ListadoMenu)
+ let Contenedor = document.getElementById('V_NavBar')
+ 
+ let Plantilla = `
+ 
+  <a class="navbar-brand" href="#">Navbar</a>
+                 <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                     aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
+                 </button>
+                 <div class="collapse navbar-collapse" id="collapsibleNavId">
+                     <ul class="navbar-nav mr-auto mt-2 mt-lg-0" id="Nav_LI">
+                     
+                        
+         
+                        
+                     </ul>
+                     <form class="form-inline my-2 my-lg-0">
+                         <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                     </form>
+                 </div>
+ 
+ `
+ 
+ Contenedor.innerHTML = Plantilla 
+
+ Datos_LI(ListadoMenu)
+ }
+ 
 
 
-///Componente NavBar
-
-Mostar_NavBar ();
-
-// Let COntenedor =  ID
-//let Contenido = "Hola mundo"
-//Contenedor.innerHTML = Contenido
-function Mostar_NavBar () {
-    // let Opcion1 = "Inicio";
-    // let Opcion2 = "Listado de Productos";
-    // let Opcion3 = "Crear Producto";
-    // let Opcion4 = "Pagina de Mercadeo";
-    // let Menu = {
-    //     Nombre : "Inicio",
-    //     Ruta: " Index.html"
-    // }
-
-    //Arreglo de objetos [{},{},{}]
-    
-
-    let ListadoMenu =[
-        {
-            Nombre : "Inicio",
-            Ruta: " Index.html"
-            
-        },
-    
-        {
-            Nombre : "Listado de Productos",
-            Ruta: "List-Products.html"
-        },
-
-        {
-            Nombre : "Crear Producto",
-            Ruta: "New-Product.html"
-        },
-
-        {
-            Nombre : "Pagina de Mercadeo",
-            Ruta: " #"
-        },
-    
-
-    ]
-
-    //Bucles o Ciclos
-    console.log (ListadoMenu)
-    let Contenedor = document.getElementById('V_NavBar')
-
-    let PLantilla  = `
-    <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse"
-                data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="collapsibleNavId">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="${ListadoMenu[0].Ruta}">${ListadoMenu[0].Nombre} <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="${ListadoMenu[1].Ruta}">${ListadoMenu[1].Nombre}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href=${ListadoMenu[2].Ruta}>${ListadoMenu[2].Nombre}</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="${ListadoMenu[3].Ruta}">${ListadoMenu[3].Nombre}</a>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownId">
-                            <a class="dropdown-item" href="#">Action 1</a>
-                            <a class="dropdown-item" href="#">Action 2</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" onclick="CerrarSesion()">Cerrar Sesion</a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-                </form>
-            </div>
-
-`
-Contenedor.innerHTML = PLantilla
-
-}
-
+ function Datos_LI(ListadoMenu) {
+     let contenedor2 = document.getElementById("Nav_LI")
+     ListadoMenu.forEach(element => {
+         contenedor2.innerHTML += `
+          <li class="nav-item active">
+             <a class="nav-link" href="${element.Ruta}">${element.Nombre} <span class="sr-only">(current)</span></a>
+          </li>
+         `
+     });
+ }
